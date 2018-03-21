fa230511cf90af081d73a06367a560629dacc99d
Lebedev Roman <lebedeffroman@gmail.com>
2018-03-21 18:48:12 +0300

    Fix simple grammar
    Remove redundant params in grammar.
    Fix equality and hashing.

--- src/main/kotlin/ctd/lebedev/dev/grammar/Expression.kt
+++ src/main/kotlin/ctd/lebedev/dev/grammar/Expression.kt
@@ -4,30 +4,59 @@ sealed class Expr {
     abstract fun eval(vars: MutableMap<String, Boolean>): Boolean
 }
 
-sealed class UnaryExpr(var exp: Expr) : Expr()
-sealed class BinaryExpr(var l: Expr, var r: Expr) : Expr()
+sealed class UnaryExpr(var expr: Expr) : Expr() {
+    override fun equals(other: Any?): Boolean {
+        if (other is UnaryExpr) return expr == other.expr
+        return super.equals(other)
+    }
 
-data class Impl(var lhs: Expr, var rhs: Expr) : BinaryExpr(lhs, rhs) {
+    override fun hashCode(): Int {
+        return expr.hashCode()
+    }
+}
+
+sealed class BinaryExpr(var l: Expr, var r: Expr) : Expr() {
+    override fun equals(other: Any?): Boolean {
+        if (other is BinaryExpr) return l == other.l && r == other.r
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int {
+        var result = l.hashCode()
+        result = 31 * result + r.hashCode()
+        return result
+    }
+}
+
+class Impl(l: Expr, r: Expr) : BinaryExpr(l, r) {
     override fun toString() = "($l->$r)"
     override fun eval(vars: MutableMap<String, Boolean>) = !l.eval(vars) || r.eval(vars)
 }
 
-data class Or(var lhs: Expr, var rhs: Expr) : BinaryExpr(lhs, rhs) {
+class Or(l: Expr, r: Expr) : BinaryExpr(l, r) {
     override fun toString() = "($l|$r)"
     override fun eval(vars: MutableMap<String, Boolean>) = l.eval(vars) || r.eval(vars)
 }
 
-data class And(var lhs: Expr, var rhs: Expr) : BinaryExpr(lhs, rhs) {
+class And(l: Expr, r: Expr) : BinaryExpr(l, r) {
     override fun toString() = "($l&$r)"
     override fun eval(vars: MutableMap<String, Boolean>) = l.eval(vars) && r.eval(vars)
 }
 
-data class Not(var expr: Expr) : UnaryExpr(expr) {
-    override fun toString() = "(!$exp)"
-    override fun eval(vars: MutableMap<String, Boolean>) = !exp.eval(vars)
+class Not(expr: Expr) : UnaryExpr(expr) {
+    override fun toString() = "(!$expr)"
+    override fun eval(vars: MutableMap<String, Boolean>) = !expr.eval(vars)
 }
 
-data class Var(var value: String) : Expr() {
+class Var(var value: String) : Expr() {
     override fun toString() = value
     override fun eval(vars: MutableMap<String, Boolean>): Boolean = vars[value]!!
+    override fun equals(other: Any?): Boolean {
+        if (other is Var) return value == other.value
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
 }
